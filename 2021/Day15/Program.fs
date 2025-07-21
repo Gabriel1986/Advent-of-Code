043596108041c5f8245f333afaab660da1e0fe92
Mark Bracke <mark.bracke@tactics.be>
2025-07-21 19:43:37 +0200

    Optimized 2021 day 15

--- 2021/Day15/Program.fs
+++ 2021/Day15/Program.fs
@@ -1,76 +1,76 @@
 ﻿/// https://adventofcode.com/2021/day/15
 module Year2021Day15
+
 open System
 open System.Collections.Generic
 
+/// Represents a position in the maze
+[<Struct>]
+type Position = { I: int; J: int }
+
+/// Priority queue node with heuristic and total distance
+[<Struct>]
 type Node =
-    {
-        I: int
-        J: int
-        DistanceTraveled: int
-        DistanceToGoal: int
-    }
-    static member Create (i: int, j: int, previousNode: Node option, maze: int[,]) =
-        let maxI = maze.GetUpperBound(0)
-        let maxJ = maze.GetUpperBound(1)
-        {
-            I = i
-            J = j
-            DistanceTraveled = match previousNode with None -> 0 | Some x -> x.DistanceTraveled + maze[i, j]
-            DistanceToGoal =  Math.Abs (maxI - i) + Math.Abs (maxJ - j)
-        }
-    member me.Heuristic = me.DistanceTraveled + me.DistanceToGoal
-    member me.IsSameNodeAs (otherNode: Node) = otherNode.I = me.I && otherNode.J = me.J
+    val Position: Position
+    val DistanceTraveled: int
+    val Heuristic: int
 
-    member me.Neighbours (maze: int[,]) =
-        let maxI = maze.GetUpperBound(0)
-        let maxJ = maze.GetUpperBound(1)
-        let createNode (i: int, j: int) = Node.Create (i, j, Some me, maze)
+    new(pos, dist, goal) =
+        let heuristic = dist + abs (goal.I - pos.I) + abs (goal.J - pos.J)
+        { Position = pos; DistanceTraveled = dist; Heuristic = heuristic }
 
-        [
-            if me.I < maxI then createNode (me.I + 1, me.J)
-            if me.J < maxJ then createNode (me.I, me.J + 1)
-            if me.I > 0 then createNode (me.I - 1, me.J)
-            if me.J > 0 then createNode (me.I, me.J - 1)
-        ]
+let performShortestPath (maze: int[,]) =
+    let rows, cols = maze.GetLength(0), maze.GetLength(1)
+    let goal = { I = rows - 1; J = cols - 1 }
+    let visited = Array2D.create rows cols Int32.MaxValue
+    let queue = PriorityQueue<Node, int>()
+    let mutable result: int option = None
 
-//Had to use mutable collections as using immutable collections made this algorithm too slow :(
-let performShortestPathAlgorithm (maze: int[,], startNode: Node, goal: Node) =
-    let openNodes = new PriorityQueue<Node, int> ()
-    let visitedNodes = new HashSet<int * int> ()
-    let mutable minNode = startNode
+    queue.Enqueue(Node({ I = 0; J = 0 }, 0, goal), 0)
 
-    while not (minNode.IsSameNodeAs goal) do
-        visitedNodes.Add (minNode.I, minNode.J) |> ignore
-        for neighbour in minNode.Neighbours maze do
-            if (not (visitedNodes.Contains (neighbour.I, neighbour.J))) then
-                openNodes.Enqueue(neighbour, neighbour.Heuristic)
-        minNode <- openNodes.Dequeue()
+    while result.IsNone && queue.Count > 0 do
+        let current = queue.Dequeue()
+        let i, j = current.Position.I, current.Position.J
 
-    minNode.DistanceTraveled
+        if current.Position = goal then
+            result <- Some current.DistanceTraveled
+        else
+            if current.DistanceTraveled >= visited[i, j] then
+                () // already visited with shorter distance
+            else
+                visited[i, j] <- current.DistanceTraveled
 
-let private parseMaze (input: string array) =
-    Array2D.init input.Length input[0].Length (fun i j -> int (string (input[i][j])))
+                for di, dj in [(-1,0); (1,0); (0,-1); (0,1)] do
+                    let ni, nj = i + di, j + dj
+                    if ni >= 0 && nj >= 0 && ni < rows && nj < cols then
+                        let cost = current.DistanceTraveled + maze[ni, nj]
+                        if cost < visited[ni, nj] then
+                            let neighbor = Node({ I = ni; J = nj }, cost, goal)
+                            queue.Enqueue(neighbor, neighbor.Heuristic)
 
-let part1 (input) =
-    let maze = parseMaze (input)
-    let maxI = maze.GetUpperBound 0
-    let maxJ = maze.GetUpperBound 1
-    performShortestPathAlgorithm (maze, Node.Create (0, 0, None, maze), Node.Create (maxI, maxJ, None, maze))
+    if result.IsNone then 
+        failwith "Goal not reachable"
+    else 
+        result.Value
 
-let private expand (maze: int[,]): int[,] =
-    Array2D.init ((maze.GetUpperBound(0) + 1) * 5) ((maze.GetUpperBound(1) + 1) * 5) (fun i j ->
-        let originalI = i % (maze.GetUpperBound(0) + 1)
-        let originalJ = j % (maze.GetUpperBound(1) + 1)
-        let iterationI = i / (maze.GetUpperBound(0) + 1)
-        let iterationJ = j / (maze.GetUpperBound(1) + 1)
-        let calculated = maze[originalI, originalJ] + iterationI + iterationJ
-        if calculated > 9 then calculated - 9 else calculated
-    )
+let parseMaze (input: string[]) =
+    Array2D.init input.Length input[0].Length (fun i j -> int (string input.[i].[j]))
 
-let part2 (input: string array) =
-    let maze = parseMaze (input)
-    let expandedMaze = expand maze
-    let maxI = expandedMaze.GetUpperBound 0
-    let maxJ = expandedMaze.GetUpperBound 1
-    performShortestPathAlgorithm (expandedMaze, Node.Create (0, 0, None, expandedMaze), Node.Create (maxI, maxJ, None, expandedMaze))
+let part1 input =
+    input
+    |> parseMaze
+    |> performShortestPath
+
+let expand (maze: int[,]) =
+    let height, width = maze.GetLength(0), maze.GetLength(1)
+    Array2D.init (height * 5) (width * 5) (fun i j ->
+        let orig = maze[i % height, j % width]
+        let added = i / height + j / width
+        let value = orig + added
+        if value > 9 then value - 9 else value)
+
+let part2 input =
+    input 
+    |> parseMaze 
+    |> expand 
+    |> performShortestPath
